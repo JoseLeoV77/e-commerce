666d2411a7c60880a7082e2a9294f747fdfa250f
JoseLeoV77 <172601507+JoseLeoV77@users.noreply.github.com>
2025-09-08 10:13:19 -0400

    slider css

--- client/src/utils/images.ts
+++ client/src/utils/images.ts
@@ -2,11 +2,6 @@ export const images = [
   {src:'../../../src/assets/images/gamingpicture.jpg' , alt: 'gaming', desc: 'Get your game on!'},
   {src:'../../../src/assets/images/products.jpg' , alt: 'beauty', desc: 'Beauty essentials!'},
   {src: '../../../src/assets/images/furnitures.jpg', alt: 'furniture' , desc: 'Furniture products!'},
-  {src: '../../../src/assets/images/furnitures.jpg', alt: 'furniture' , desc: 'Furniture products!'},
-  {src: '../../../src/assets/images/furnitures.jpg', alt: 'furniture' , desc: 'Furniture products!'},
-  {src: '../../../src/assets/images/furnitures.jpg', alt: 'furniture' , desc: 'Furniture products!'},
-  {src: '../../../src/assets/images/furnitures.jpg', alt: 'furniture' , desc: 'Furniture products!'},
-  {src: '../../../src/assets/images/furnitures.jpg', alt: 'furniture' , desc: 'Furniture products!'},
 ]
 
 export const sliderImages = [
